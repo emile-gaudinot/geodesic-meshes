67fb4b14d76c0a3da934a7fe645dbc42dbd81f3c
emile-gaudinot <123317321+emile-gaudinot@users.noreply.github.com>
2025-08-12 23:06:22 +0200

    Create transform_csv.py

---
+++ transform_csv.py
@@ -0,0 +1,36 @@
+"""
+Transforme un fichier .csv de nuage de points avec tabulations en fichier .csv classique
+avec des virgules
+"""
+
+import csv
+
+
+def conversion(nomSansExtension):
+    fileCsv = open(nomSansExtension + '.csv')
+    csvreader = csv.reader(fileCsv)
+    fileTxt = open(nomSansExtension + 'SansTabulation.csv', 'w')
+    for row in csvreader:
+        if row != []: # dernière ligne est vide
+            ligne = row[0]
+            nouvelleLigne = ''
+            i = 0
+            for j in range(3):
+                mot = ''
+                while ligne[i] != '\t':
+                    mot += ligne[i]
+                    i += 1
+                nouvelleLigne += mot
+                if j != 2:
+                    nouvelleLigne += ','
+                i += 1
+
+            #on a récup les coordonnées
+            fileTxt.write(nouvelleLigne)
+            fileTxt.write('\n')
+
+    fileCsv.close()
+    fileTxt.close()
+  
+nom = 'resultline1'
+conversion(nom)
